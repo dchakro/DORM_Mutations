67f8e4c7dca9385c4a3ee46e52f05a1af4f4384a
robocopAlpha <deep@iMac.T5>
2020-07-07 12:58:44 +0300

    Complete updated data frame.

--- server.R
+++ server.R
@@ -1,25 +1,12 @@
-# Load the ggplot2 package which provides
-# the 'mpg' dataset.
-# library(ggplot2)
 library(shiny)
 library(shinythemes)
 
-DF <- readRDS("20200703.FrequencyByResidue.RDS")
-DF <- DF[1:10000,]
-colnames(DF)[1] <- "Gene Residue"
-colnames(DF)[2]  <- "Total Cases"
-colnames(DF)[3] <- "Number of mutations in different tissues"
+DF <- readRDS("20200706.FrequencyByResidue.RDS")
+colnames(DF)[3]  <- "Total Cases"
+colnames(DF)[4] <- "Number of mutations in different tissues"
 
 function(input, output,session) {
-#   output$HotspotPlot <- renderImage({
-#     return(list(
-#       src = "Hotspot_resize.jpg",
-#       contentType = "image/jpg",
-#       alt = "Hotspot Plot"
-#     ))
-#   }, deleteFile = FALSE) 
-  # Filter data based on selections
-  TwitterURL <- a("@DeepOnCar",
+ TwitterURL <- a("@DeepOnCar",
                   href="https://twitter.com/DeepOnCar")
   output$tweetURL <- renderUI({tagList("Find the developer:",TwitterURL,"on twitter.")})
   output$table <- DT::renderDataTable(
@@ -30,6 +17,7 @@ function(input, output,session) {
                      columnDefs = list(list(width = '120px', targets = c(0)))),
       rownames = FALSE 
   ))
+  # To-Do
   # Add download button to filtered datatable()
   # https://stackoverflow.com/questions/41597062/r-download-filtered-datatable
   session$onSessionEnded(function() {
