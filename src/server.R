fdceb313c6dafcb5ddc9a9afa52100f3c3ab0a69
dchakro <35454738+dchakro@users.noreply.github.com>
2020-09-19 00:12:20 +0300

    Working to generate realtime plots

--- src/server.R
+++ src/server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinythemes)
-
+if(!file.exists("../data/Table.csv")){
+  system(command = "xz -kd ../data/Table.csv.xz",intern = F,wait = T)
+}
 
 # DF <- readRDS("../data/20200819.FrequencyByMutation.RDS")
 # write(colnames(DF),file = "../data/ColumnNames.txt",ncolumns = 1)
@@ -18,53 +20,42 @@ library(shinythemes)
 # colnames(DF)[3]  <- "Total Cases"
 # colnames(DF)[4] <- "Number of mutations in different tissues"
 function(input, output,session) {
-  observeEvent(input$RunSearch,{
-    searchTerm <- input$Search
-    plotSize <- input$size
-    output$options <- renderPrint(input$Search)
-    if(searchTerm == ""){
-      command <- ""
-      DF <- data.frame(vroom::vroom("../data/Table.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt")),stringsAsFactors = F)
-      output$table <- DT::renderDataTable(DT::datatable(data = DF, options(list(rownames = FALSE))))
-      output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = "Mutations",ylab="Frequency",col = "#ff5e19")})
-    } else {
-      if(grepl(pattern = " ",x = searchTerm,fixed = T)){
-        searchTerm <- unlist(strsplit(x = searchTerm, split = " ",fixed = T),use.names = F)
-        command <- paste0("grep -i ",searchTerm[1]," ../data/Table.csv | ",paste0("grep -i ",searchTerm[2:length(searchTerm)],collapse = " | "), " >| ../tmp/tmp.csv")
+  RunAnalysis <- reactive({list(input$Search,input$size)})
+    observeEvent(RunAnalysis(),{
+      searchTerm <- input$Search
+      plotSize <- as.integer(ifelse(test = (input$size == "all"),
+                          yes = 653684,
+                          no = input$size))
+      # output$options <- renderPrint(input$Search)
+      if(searchTerm == ""){
+        command <- ""
+        DF <- data.frame(vroom::vroom("../data/Table.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
+        plotSize <- min(plotSize,nrow(DF),na.rm = T)
+        output$table <- renderTable(DF[1:plotSize,])
+        output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = paste0("Mutations (n=",plotSize,")"),ylab="Frequency",col = "#ff5e19",family="serif")})
       } else {
-        if(searchTerm != "") command = paste0("grep -i ",searchTerm," ../data/Table.csv >| ../tmp/tmp.csv")
+        if(grepl(pattern = " ",x = searchTerm,fixed = T)){
+          searchTerm <- unlist(strsplit(x = searchTerm, split = " ",fixed = T),use.names = F)
+          command <- paste0("grep -i '",searchTerm[1],"' ../data/Table.csv | ",paste0("grep -i '",searchTerm[2:length(searchTerm)],"'",collapse = " | "), " >| ../tmp/tmp.csv")
+        } else {
+          if(searchTerm != "") command = paste0("grep -i '",searchTerm,"' ../data/Table.csv >| ../tmp/tmp.csv")
+        }
+        if(command != "" ){
+          print(command)
+          # output$options <- renderPrint(command)
+          system(command = command, intern = F, wait=T)
+          DF <- data.frame(vroom::vroom("../tmp/tmp.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
+          # print(dim(DF))
+          # print(min(c(plotSize,nrow(DF)),na.rm = T))
+          #print(plotSize)
+          # print(DF[1:plotSize,])
+          plotSize <- min(c(plotSize,nrow(DF)),na.rm = T)
+          output$table <- renderTable(DF[1:plotSize,])
+          output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = paste0("Mutations (n=",plotSize,")"),ylab="Frequency",col = "#ff5e19",family="serif")})
+          # system(command = "rm ../tmp/tmp.csv",intern = F,wait=T)
+        }
       }
-      if(command != "" ){
-        print(command)
-        output$options <- renderPrint(command)
-        system(command = command,intern = F)
-        DF <- data.frame(vroom::vroom("../tmp/tmp.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt")),stringsAsFactors = F)
-        output$table <- DT::renderDataTable(DT::datatable(data = DF, options(list(rownames = FALSE))))
-        # system(command = "rm ../tmp/tmp.csv",intern = F)
-        output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = "Mutations",ylab="Frequency",col = "#ff5e19")})
-      }
-    }
 })
-    
-    
-    # DF.shiny$m <- data.frame(vroom::vroom("../tmp/tmp.csv",col_names = readLines("../data/ColumnNames.txt")),stringsAsFactors = F)
-    # tmp <- DF[!is.na(DF[,column_idx]),]
-    # DF.shiny$m <- tmp[order(tmp[,(3+seq_len(column_idx))],decreasing = T),]
-  
-  # output$table <- reactive(DT::renderDataTable(
-  #   DT::datatable(
-  #     data <- DF[,input$"TissueTypes"],
-  #     options = list(lengthMenu = c(5, 10, 50, 100, 200), 
-  #                    pageLength = 10 ,
-  #                    columnDefs = list(list(width = '120px', targets = c(0)))),
-  #     rownames = FALSE
-  #     )
-  #   ))
-  
-  # To-Do
-  # Add download button to filtered datatable()
-  # https://stackoverflow.com/questions/41597062/r-download-filtered-datatable
-  
   session$onSessionEnded(function() {
         gc()
         stopApp()
