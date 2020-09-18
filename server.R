aca6bfb21ba4071f0c4977c91394ad4b4deaa555
dchakro <35454738+dchakro@users.noreply.github.com>
2020-09-19 00:21:06 +0300

    Moved Shiny app files

--- server.R
+++ server.R
@@ -1,16 +1,16 @@
 library(shiny)
 library(shinythemes)
-if(!file.exists("../data/Table.csv")){
-  system(command = "xz -kd ../data/Table.csv.xz",intern = F,wait = T)
+if(!file.exists("./data/Table.csv")){
+  system(command = "xz -kd ./data/Table.csv.xz",intern = F,wait = T)
 }
 
-# DF <- readRDS("../data/20200819.FrequencyByMutation.RDS")
-# write(colnames(DF),file = "../data/ColumnNames.txt",ncolumns = 1)
-# write.table(x = DF,file = "../data/Table.csv",col.names = F,row.names = F,sep = ";",quote = F)
+# DF <- readRDS("./data/20200819.FrequencyByMutation.RDS")
+# write(colnames(DF),file = "./data/ColumnNames.txt",ncolumns = 1)
+# write.table(x = DF,file = "./data/Table.csv",col.names = F,row.names = F,sep = ";",quote = F)
 
-# DF <- readRDS("../data/20200918.Frequency_byTissue.RDS")
-# write(colnames(DF),file = "../data/ByTissue.ColumnNames.txt",ncolumns = 1)
-# write.table(x = DF,file = "../data/ByTissue.table.csv",col.names = F,row.names = F,sep = ";",quote = F)
+# DF <- readRDS("./data/20200918.Frequency_byTissue.RDS")
+# write(colnames(DF),file = "./data/ByTissue.ColumnNames.txt",ncolumns = 1)
+# write.table(x = DF,file = "./data/ByTissue.table.csv",col.names = F,row.names = F,sep = ";",quote = F)
 
 # searchTerm <- "KRAS G12"
 
@@ -29,22 +29,22 @@ function(input, output,session) {
       # output$options <- renderPrint(input$Search)
       if(searchTerm == ""){
         command <- ""
-        DF <- data.frame(vroom::vroom("../data/Table.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
+        DF <- data.frame(vroom::vroom("./data/Table.csv",delim = ";",col_names = readLines("./data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
         plotSize <- min(plotSize,nrow(DF),na.rm = T)
         output$table <- renderTable(DF[1:plotSize,])
         output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = paste0("Mutations (n=",plotSize,")"),ylab="Frequency",col = "#ff5e19",family="serif")})
       } else {
         if(grepl(pattern = " ",x = searchTerm,fixed = T)){
           searchTerm <- unlist(strsplit(x = searchTerm, split = " ",fixed = T),use.names = F)
-          command <- paste0("grep -i '",searchTerm[1],"' ../data/Table.csv | ",paste0("grep -i '",searchTerm[2:length(searchTerm)],"'",collapse = " | "), " >| ../tmp/tmp.csv")
+          command <- paste0("grep -i '",searchTerm[1],"' ./data/Table.csv | ",paste0("grep -i '",searchTerm[2:length(searchTerm)],"'",collapse = " | "), " >| ./tmp/tmp.csv")
         } else {
-          if(searchTerm != "") command = paste0("grep -i '",searchTerm,"' ../data/Table.csv >| ../tmp/tmp.csv")
+          if(searchTerm != "") command = paste0("grep -i '",searchTerm,"' ./data/Table.csv >| ./tmp/tmp.csv")
         }
         if(command != "" ){
           print(command)
           # output$options <- renderPrint(command)
           system(command = command, intern = F, wait=T)
-          DF <- data.frame(vroom::vroom("../tmp/tmp.csv",delim = ";",col_names = readLines("../data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
+          DF <- data.frame(vroom::vroom("./tmp/tmp.csv",delim = ";",col_names = readLines("./data/ColumnNames.txt"),col_types = c(counts="i")),stringsAsFactors = F)
           # print(dim(DF))
           # print(min(c(plotSize,nrow(DF)),na.rm = T))
           #print(plotSize)
@@ -52,7 +52,7 @@ function(input, output,session) {
           plotSize <- min(c(plotSize,nrow(DF)),na.rm = T)
           output$table <- renderTable(DF[1:plotSize,])
           output$plot <- renderPlot({barplot(DF$counts[1:plotSize],xlab = paste0("Mutations (n=",plotSize,")"),ylab="Frequency",col = "#ff5e19",family="serif")})
-          # system(command = "rm ../tmp/tmp.csv",intern = F,wait=T)
+          # system(command = "rm ./tmp/tmp.csv",intern = F,wait=T)
         }
       }
 })
