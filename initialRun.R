8dfde32eb105daa776272d3bbe1a5dbedafd73dc
dchakro <35454738+dchakro@users.noreply.github.com>
2021-06-10 11:11:36 +0300

    Increase verbosity and speed

--- initialRun.R
+++ initialRun.R
@@ -2,6 +2,7 @@ rm(list=ls())
 # -- Create essential files (initial run)
 DF <- readRDS("./data/20210603.FrequencyMutations.byTissue.RDS")
 write(colnames(DF),file = "./data/ColumnNames.txt",ncolumns = 1)
+message("Writing RDS as CSV")
 write.table(x = DF,file = "./data/RAW_DATA.csv",col.names = F,row.names = F,sep = ";",quote = F)
 
 tissues <- readLines("./data/ColumnNames.txt",warn = F)
@@ -10,16 +11,23 @@ tissues <- tissues[tissues %nin% c("Gene","Mutation","counts","Frequency")]
 
 dir.create("./data/tissue")
 file.copy("./data/RAW_DATA.csv","./data/tissue/all.csv")
-for(tissue in tissues){
+
+writeTissueCSV <- function(tissue){
   sub <- na.omit(DF[,c(1,2,which(colnames(DF) == tissue))])
   sub <- sub[order(sub[,3],decreasing = T),]
+  outfile <- paste0("./data/tissue/",tissue,".csv")
+  message(paste("Writing",outfile))
   write.table(x = sub,
-              file = paste0("./data/tissue/",tissue,".csv"),
+              file = outfile,
               col.names = F,
               row.names = F,
               sep = ";",
               quote = F)
+  return(NULL)
 }
 
+invisible(lapply(X = tissues,FUN = function(x) writeTissueCSV(x)))
+
+message("Done!")
 message("Remember to create ./tmp directory")
 message("Then, run sudo chown -R shiny:shiny tmp ")
