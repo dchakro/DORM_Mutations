b54247fa6439fdb42cd4f93f12a544301d76c290
dchakro <35454738+dchakro@users.noreply.github.com>
2022-01-03 13:50:19 +0200

    Info about bookmarking server state

--- global.R
+++ global.R
@@ -1,1 +1,4 @@
+# Enables server-aided bookmarking of state
+# Bookmarks on the server at /var/lib/shiny-server/bookmarks
+# More info: https://shiny.rstudio.com/articles/bookmarking-state.html
 enableBookmarking(store = "server")
