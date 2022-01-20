14d8364f17cd978580390fd961c3b5039ec8740f
dchakro <35454738+dchakro@users.noreply.github.com>
2022-01-20 09:40:41 +0200

    Show 50 records by default, note about recurrent = n>1

--- ui.R
+++ ui.R
@@ -38,7 +38,7 @@ fluidPage(
                selectInput("size",
                    "No. of records:",
                    c(10,50,100,500,1000,10000),
-                   selected = "100",
+                   selected = "50",
                    multiple = F,
                    selectize = F,
                    width = "120")
@@ -68,6 +68,8 @@ fluidPage(
 	              # The settings and search terms are anonymously saved to the server.
   hr(),
 	p(strong("Tip:"),'You can use a comma ( , ) or a semicolon ( ; ) to query multiple proteins e.g.', strong(span("BRAF, KRAS, EGFR",style='color:#EE5F21')),
+	  align="left"),
+	p(strong("Note:"),'The database only lists recurrent mutations (i.e. mutations with population frequency > 1) idenfied from genomes of cancer samples.',
 	  align="left"),
   hr(),
   tableOutput("table"),
