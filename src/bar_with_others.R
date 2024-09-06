08d9221a66fa714106cda8cc23b97dcf68fa560b
kvaparanta <61691402+kvaparanta@users.noreply.github.com>
2024-09-06 14:36:50 +0300

    Update bar_with_others.R
    updated the script to call the new count file

--- src/bar_with_others.R
+++ src/bar_with_others.R
@@ -114,7 +114,7 @@ plot_bar <- function(Tissue){
 setwd("/Users/deepankar/OneDrive - O365 Turun yliopisto/Git/GitHub/websites/eleniuslabtools.utu.fi/shiny-apps/DORM/Mutations/src")
 source("https://gist.githubusercontent.com/dchakro/8b1e97ba6853563dd0bb5b7be2317692/raw/parallelRDS.R")
 
-Stats <- readRDS.gz("/Users/deepankar/OneDrive - O365 Turun yliopisto/ExtraWorkSync/Klaus-Lab-Data/Big Data/COSMIC/v96/CountStatsRAW.RDS")
+Stats <- readRDS.gz("C:/Users/katri/seadrive_root/Katri Va/Shared with me/DORM database/Data/CountStatsRAW.RDS")
 rm(loadRDS,readRDS.gz,writeRDS,saveRDS.gz)
 
 sampleCount <- unique(Stats[,.(Sample.name,tissue)])[,.N,.(tissue)]
